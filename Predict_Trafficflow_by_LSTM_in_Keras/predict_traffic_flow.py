0f8d34a827e664f06d404fe3831808dcf13f00bd
Federico Karagulian <karafede@hotmail.com>
2019-10-25 15:24:55 +0200

    Add files via upload

--- Predict_Trafficflow_by_LSTM_in_Keras/predict_traffic_flow.py
+++ Predict_Trafficflow_by_LSTM_in_Keras/predict_traffic_flow.py
@@ -9,7 +9,9 @@ from sklearn.metrics import mean_absolute_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+import pandas as pd
 import os
+import matplotlib as mpl
 os.getcwd()
 
 # parameters settings
@@ -17,11 +19,14 @@ files_dir = "C:\\python\\projects\\giraffe\\Predict_Trafficflow_by_LSTM_in_Keras
 with_weekends = True
 epoch_num = 2
 
-data_train, data_test = \
+# data and DATEs...
+data_train, data_test, DATE_data_train, DATE_data_test = \
     read_traffic_flow.read_traffic_flow(files_dir, with_weekends)
 
-data_train_ori = data_train.reshape(data_train.shape[0], -1).astype('float32')
+data_train_original = data_train.reshape(data_train.shape[0], -1).astype('float32')
 data_test = data_test.reshape(data_test.shape[0], -1).astype('float32')
+
+'''
 plt.figure(1)
 plt.plot(data_train)
 # plt.figure(2)
@@ -29,35 +34,67 @@ plt.plot(data_train)
 plt.figure(3)
 plt.plot(data_test)
 plt.show()
+'''
+
+# plot time-series with dates
+TS = pd.DataFrame({'date_time': DATE_data_train, 'flow': data_train})
+TS = TS.sort_values(by='date_time')
+print(TS.info())
+print(type(TS))
+print(TS.columns)
+
+# field datetime is a "type" object...but we want this as index (for the date_time structure)
+# remove seconds
+TS['datetime'] = pd.to_datetime(TS['date_time'], format='%m/%d/%Y %H:%M')
+print(type(TS))
+TS = TS.set_index('date_time')
+TS.drop(['datetime'], axis=1, inplace=True)
+print(TS.columns)
+
+'''
+# plot Time Series
+TS.plot(figsize=(20,8), linewidth=4, fontsize=8, rot=45)
+plt.xlabel('datetime', fontsize=20)
+plt.show()
+'''
 
 # create 2 arrays of data shifted by 1 time-stamp (5 minutes)
-def create_dataset(dataset, look_back = 1):
-    data_x, data_y = [], []
+# this is the so-called windowed cross-sectional conversion with a time window of 5 miuntes
+def create_dataset(dataset, timestamps, look_back = 1):
+    data_x, data_y, time_x, time_y = [], [], [], []
     for i in range(len(dataset)-look_back-1):
         data_x.append(dataset[i:(i+look_back)])
+        time_x.append(timestamps[i:(i+look_back)])
         data_y.append(dataset[i + look_back])
-    return np.array(data_x), np.array(data_y)
+        time_y.append(timestamps[i + look_back])
+    return np.array(data_x), np.array(data_y), \
+           np.array(time_x), np.array(time_y)
 
 
 # fix random seed for reproducibility
 np.random.seed(7)
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
-data_train = scaler.fit_transform(data_train_ori)
+data_train = scaler.fit_transform(data_train_original)
 data_test = scaler.fit_transform(data_test)
+
+# RESHUFFLE the training data (mix the data)
+# np.random.shuffle(data_train)
 
 look_back = 1  # it is the same thing of the lag
 # train_x and train_y are shifted by 1 time stamp that is 5 minutes
-train_x, train_y = create_dataset(data_train, look_back)
-test_x, test_y = create_dataset(data_test, look_back)
+train_x, train_y, time_train_x, time_train_y = create_dataset(data_train, DATE_data_train, look_back)
+test_x, test_y, time_test_x, time_test_y = create_dataset(data_test, DATE_data_test, look_back)
 
 train_x = train_x.reshape(train_x.shape[0], 1, train_x.shape[1])
 test_x = test_x.reshape(test_x.shape[0], 1, test_x.shape[1])
 
+
 # create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, look_back)))
-model.add(Dense(1))
+model.add(LSTM(4, input_shape=(1, look_back))) # 4 neurons, 1 layers
+# model.add(LSTM(256, input_shape=(1, look_back))) # 4 neurons by 64 layers
+model.add(Dense(1))  # add only one layer
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(train_x, train_y,
           epochs=epoch_num, batch_size=1)  # ,verbose = 2
@@ -66,11 +103,11 @@ model.fit(train_x, train_y,
 #       epochs=config["epochs"],
 #       validation_split=0.05)
 
-# make predictions
+# make predictions with the obtained model
 trainPredict = model.predict(train_x)  # close to train_y
 testPredict = model.predict(test_x)    # close to test_y
 
-# invert predictions (go back to real data)
+# invert predictions (go back to real data....from normalized)
 trainPredict = scaler.inverse_transform(trainPredict)
 train_y = scaler.inverse_transform([train_y.ravel()])
 testPredict = scaler.inverse_transform(testPredict)
@@ -87,26 +124,139 @@ print('Test Score: %.2f RMSE' % (testScore))
 
 # new time-series with length = train data + test data (make an empty array)
 l = data_train.shape[0] + data_test.shape[0]
-# # shift train predictions for plotting
+# shift train predictions for plotting
 trainPredictPlot = np.empty((l, 1))
 trainPredictPlot[:, :] = np.nan
 # populate the first part of the time series with the train-predicted data
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
 
-# # shift test predictions for plotting (this is the new forecasted part of the time-series
+# shift test predictions for plotting (this is the new forecasted part of the time-series
 testPredictPlot = np.empty((l, 1))
 testPredictPlot[:, :] = np.nan
 testPredictPlot[len(trainPredict)+look_back*3:l-1, :] = testPredict
 
 test_y = test_y + 1
 
+# differences between predicted values and test values
 loss_test = np.sum(np.abs(testPredict - test_y) / test_y) / test_y.shape[0]
 
-# plot baseline and predictions
+#####################################################################
+################## plot train and test data #########################
+
+'''
+# plot baseline and predictions (TOTAL data)
 plt.plot(scaler.inverse_transform(
     np.vstack((data_train, data_test))), 'yellow')
+'''
+
+all_DATES = np.concatenate((DATE_data_train, DATE_data_test))
+# all_DATES = all_DATES.reshape(all_DATES.shape[0],-1)
+
+data_total = scaler.inverse_transform(np.vstack((data_train, data_test))).round(0).T
+data_total = data_total.ravel()
+data_trainPrediction = trainPredictPlot.round(0).T.ravel()
+data_testPredict = testPredictPlot.round(0).T.ravel()
+
+
+'''
+TS_total = pd.DataFrame({'date_time': all_DATES, 'flow': data_total})
+TS_total = TS_total.sort_values(by = 'date_time')
+# remove seconds
+TS_total['datetime'] = pd.to_datetime(TS_total['date_time'], format='%m/%d/%Y %H:%M')
+TS_total = TS_total.set_index('date_time')
+TS_total.drop(['datetime'], axis=1, inplace=True)
+# plot Time Series
+TS_total.plot(figsize=(20,8), linewidth=5, fontsize=8, rot=45, color='yellow')
+plt.xlabel('datetime', fontsize=20)
+plt.show()
+
+###### plot predictions ####################################
+
 plt.plot(trainPredictPlot, 'red')
+
+
+# with DATEs...
+len(trainPredictPlot) # 7863 all dates
+data_trainPrediction = trainPredictPlot.round(0).T.ravel()
+TS_data_trainPrediction = pd.DataFrame({'date_time': all_DATES, 'flow': data_trainPrediction})
+TS_data_trainPrediction = TS_data_trainPrediction.sort_values(by = 'date_time')
+# remove seconds
+TS_data_trainPrediction['datetime'] = pd.to_datetime(TS_data_trainPrediction['date_time'], format='%m/%d/%Y %H:%M')
+TS_data_trainPrediction = TS_data_trainPrediction.set_index('date_time')
+TS_data_trainPrediction.drop(['datetime'], axis=1, inplace=True)
+# plot Time Series
+TS_data_trainPrediction.plot(figsize=(20,8), linewidth=3, fontsize=8, rot=45, color='red')
+plt.xlabel('datetime', fontsize=20)
+plt.show()
+
+
 plt.plot(testPredictPlot, 'blue')
+
+# with DATEs
+len(testPredictPlot) # 7863, all dates
+data_testPredict = testPredictPlot.round(0).T.ravel()
+TS_data_testPredict = pd.DataFrame({'date_time': all_DATES, 'flow': data_testPredict})
+TS_data_testPredict = TS_data_testPredict.sort_values(by = 'date_time')
+# remove seconds
+TS_data_testPredict['datetime'] = pd.to_datetime(TS_data_testPredict['date_time'], format='%m/%d/%Y %H:%M')
+TS_data_testPredict = TS_data_testPredict.set_index('date_time')
+TS_data_testPredict.drop(['datetime'], axis=1, inplace=True)
+# plot Time Series
+TS_data_testPredict.plot(figsize=(20,8), linewidth=5, fontsize=8, rot=45, color='blue')
+plt.xlabel('datetime', fontsize=20)
+plt.show()
+'''
+
+
+# make a unique dataframe with train+test=total data, train predict and test predict
+TS_forecast = pd.DataFrame({'date_time': all_DATES,
+                           # 'total_flow': data_total,
+                            'train_predictions': data_trainPrediction,
+                            'test_predictions': data_testPredict})
+TS_forecast = TS_forecast.sort_values(by='date_time')
+# remove seconds and transform into a time-series
+TS_forecast['datetime'] = pd.to_datetime(TS_forecast['date_time'], format='%m/%d/%Y %H:%M')
+TS_forecast = TS_forecast.set_index('date_time')
+TS_forecast.drop(['datetime'], axis=1, inplace=True)
+# TS_forecast.plot(figsize=(20,8), linewidth=3, fontsize=8, rot=45)
+
+
+# TS_forecast.total_flow.plot(figsize=(20,8), linewidth=2, fontsize=9, rot=45,color = "yellow")
+TS_forecast.train_predictions.plot(figsize=(20,8), linewidth=2, fontsize=9, rot=45, color = "red")
+TS_forecast.test_predictions.plot(figsize=(20,8), linewidth=2, fontsize=9, rot=45, color = "blue")
+plt.legend(loc='best')
+plt.xlabel(' ', fontsize=20)
+plt.show()
+
+
+'''
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+
+fig, ax = plt.subplots()
+ax.plot(TS_forecast.loc['2017-01':'2017-02', 'Consumption'], marker='o', linestyle='-')
+ax.set_ylabel('Daily Consumption (GWh)')
+ax.set_title('Jan-Feb 2017 Electricity Consumption')
+# Set x-axis major ticks to weekly interval, on Mondays
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MONDAY))
+# Format x-tick labels as 3-letter month name and day number
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'));
+
+fig, ax = plt.subplots(figsize=(20,8))
+ax.plot(TS_forecast.index.values,
+        TS_forecast['train_predictions'].values, color='purple')
+# ax.set(xlabel="Date", ylabel="Precipitation (Inches)",
+#        title="Daily Precipitation \nBoulder, Colorado 2013")
+
+# Format the x axis
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
+# ax.xaxis.set_major_formatter(DateFormatter("%m/%d/%Y %H:%M"))
+
+plt.show()
+'''
+
+
+
 plt.title("yellow: total; red: trainPred;" +
           " blue: testPred\n" +
           "If take weekends into account = {}\n".format(with_weekends) +
@@ -125,4 +275,50 @@ loss_lst = np.abs(testPredict - test_y).ravel()
 with open('./loss.txt', 'w') as fout:
     fout.writelines('\n'.join(loss_lst.astype(str).tolist()))
 
+
+######################################################################
+######################################################################
+
+# add new dates....to the time-series
+max(all_DATES)
+d = '2017-9-28 01:15'
+# make a one day length time-series
+new_DATES = pd.date_range(d, periods=288, freq='5min')
+## !!! use the symbol # % to remove the 0 leading the month and the hour
+new_DATES = new_DATES.strftime("%#m/%d/%Y %#H:%M")
+new_DATES = np.array(new_DATES)
+updated_DATES = np.concatenate((all_DATES, new_DATES))
+
+# build a new testPredictPlot AS one NEW DAY
+L = len(updated_DATES)
+new_testPredictPlot = np.empty((L, 1))
+new_testPredictPlot[:, :] = np.nan
+new_testPredictPlot[288+len(trainPredict)+look_back*3:L-1, :] = testPredict
+
+# create a trainPredictPlot but with a longer time-series of NAs
+new_trainPredictPlot = np.empty((L, 1))
+new_trainPredictPlot[:, :] = np.nan
+# populate the first part of the time series with the train-predicted data
+new_trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+
+data_trainPrediction = new_trainPredictPlot.round(0).T.ravel()
+data_testPredict = new_testPredictPlot.round(0).T.ravel()
+
+# make a unique dataframe with train+test=total data, train predict and test predict
+TS_forecast = pd.DataFrame({'date_time': updated_DATES,
+                            #'total_flow': data_total,
+                            'train_predictions': data_trainPrediction,
+                            'test_predictions': data_testPredict})
+TS_forecast = TS_forecast.sort_values(by='date_time')
+# remove seconds and transform into a time-series
+TS_forecast['datetime'] = pd.to_datetime(TS_forecast['date_time'], format='%m/%d/%Y %H:%M')
+TS_forecast = TS_forecast.set_index('date_time')
+TS_forecast.drop(['datetime'], axis=1, inplace=True)
+# TS_forecast.plot(figsize=(20,8), linewidth=3, fontsize=8, rot=45)
+
+
+TS_forecast.train_predictions.plot(figsize=(20,8), linewidth=2, fontsize=9, rot=45, color = "red")
+TS_forecast.test_predictions.plot(figsize=(20,8), linewidth=2, fontsize=9, rot=45, color = "blue")
+plt.legend(loc='best')
+plt.xlabel(' ', fontsize=20)
 plt.show()
--- Predict_Trafficflow_by_LSTM_in_Keras/read_traffic_flow.py
+++ Predict_Trafficflow_by_LSTM_in_Keras/read_traffic_flow.py
@@ -14,6 +14,8 @@ def read_traffic_flow(files_dir, with_weekends=True):
     traffic_flow = []
     traffic_flow_train = []
     traffic_flow_test = []
+    DATE_traffic_flow_train = []
+    DATE_traffic_flow_test = []
     # join 5 xlsx files
     xlsxes = [os.path.join(files_dir, i) for i in os.listdir(files_dir)
               if i[-5:] == ".xlsx"]
@@ -24,48 +26,58 @@ def read_traffic_flow(files_dir, with_weekends=True):
     traffic_flow = np.vstack(traffic_flow)
     dates = []
     for date in traffic_flow[:, 0]:
+        # date = traffic_flow[5000, 0]
         # get only the DAY
         date = date.split()[0]
         if date not in dates:
             dates.append(date)
-    # REMOVE WEEK-ENDS
     if not with_weekends:
-        # select a day that is a Monday
-        first_date = datetime.datetime.strptime('8/28/2017', '%m/%d/%Y')  # Monday
-        for i_data in range(traffic_flow.shape[0]-1, -1, -1):
+        first_date = datetime.datetime.strptime('8/28/2017', '%m/%d/%Y')  # Mon
+        for i_data in range(traffic_flow.shape[0] - 1, -1, -1):
+            # i_data = 5000
             curr_day = traffic_flow[i_data][0].split()[0]
             if not with_weekends:
                 curr_date = datetime.datetime.strptime(
                     traffic_flow[i_data][0].split()[0], '%m/%d/%Y')
-                if (curr_date - first_date).days % 7 > 4:   # divided by 7
+                if (curr_date - first_date).days % 7 > 4:
                     traffic_flow = np.delete(traffic_flow, i_data, 0)
-                    # print("curr_day removed:", curr_day)
+                    print("curr_day removed:", curr_day)
                     if curr_day in dates:
                         dates.remove(curr_day)
-    # print("dates:", dates)
+    print("dates:", dates)
     for data in traffic_flow:
+        # data = traffic_flow[5000,:]
         curr_day = data[0].split()[0]
         # if curr_day == dates[-3]:
         #     traffic_flow_validate.append(data[1])
         if curr_day == dates[-2]:
+            # test data can only be of 1 day length
             traffic_flow_test.append(data[1])
+            DATE_traffic_flow_test.append(data[0])
         elif curr_day == dates[-1]:
             continue
         else:
             traffic_flow_train.append(data[1])
+            DATE_traffic_flow_train.append(data[0])
 
+    # traffic_flow_test = traffic_flow[2000:3000,1]
     traffic_flow_train = np.array(traffic_flow_train)
     traffic_flow_test = np.array(traffic_flow_test)
+    DATE_traffic_flow_train = np.array(DATE_traffic_flow_train)
+    DATE_traffic_flow_test = np.array(DATE_traffic_flow_test)
+    # DATE_traffic_flow_test = traffic_flow[2000:3000,0]
     print("read_traffic_flow " + files_dir + "finished!")
-    return traffic_flow_train, traffic_flow_test
+    return traffic_flow_train, traffic_flow_test, \
+           DATE_traffic_flow_train, DATE_traffic_flow_test
 
 
 def main():
     files_dir = "C:\\python\\projects\\giraffe\\Predict_Trafficflow_by_LSTM_in_Keras"
-    data_train, data_test = read_traffic_flow(files_dir)
-    print("traffic_flow_train.shape = " + str(data_train.shape))
-    print("traffic_flow_test.shape = " + str(data_test.shape))
+    data_train, data_test, DATE_data_train, DATE_data_test = read_traffic_flow(files_dir)
+    print("traffic_flow_train.shape = " + str(data_train.shape), str(DATE_data_train.shape))
+    print("traffic_flow_test.shape = " + str(data_test.shape),str(DATE_data_test.shape) )
 
 
 if __name__ == '__main__':
     main()
+
